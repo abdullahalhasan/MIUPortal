d358ffd2ffa34d307ea05b7744a4b0a20f6c3138
abdullahalhasan <hshasan224@gmail.com>
2017-02-25 20:31:19 +0600

    Added Drawer oprtion in drawer there is developer website and share button Added internet connection checking by Broadcast Added Firebase Added Admobs Added Round Image Classes

--- app/src/main/java/me/hasan/miuportal/Home.java
+++ app/src/main/java/me/hasan/miuportal/Home.java
@@ -1,30 +1,108 @@
 package me.hasan.miuportal;
 
-import android.content.DialogInterface;
-import android.net.ConnectivityManager;
-import android.os.Handler;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawer;
     private WebView webView;
     private ConnectionDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_nav);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+
+        adView.loadAd(adRequest);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        webInit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav, menu);
+        return true;
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_web) {
+            loadWeb();
+        } else if (id == R.id.nav_portal) {
+            loadPortal();
+        } else if (id == R.id.nav_developer) {
+            Intent intent = new Intent(this,Developer.class);
+            startActivity(intent);
+        }  else if (id == R.id.nav_share) {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            String shareBody = "https://play.google.com/store/apps/details?id=me.hasan.miuportal&hl=en";
+            String shareSubject = "MIU Portal";
+
+            intent.putExtra(Intent.EXTRA_SUBJECT,shareSubject);
+            intent.putExtra(Intent.EXTRA_TEXT,shareBody);
+
+            startActivity(Intent.createChooser(intent,"Share Using"));
+        }
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void webInit() {
         webView = (WebView) findViewById(R.id.webView);
 
         WebSettings webSettings = webView.getSettings();
@@ -32,24 +110,20 @@ public class Home extends AppCompatActivity {
         webSettings.setBuiltInZoomControls(true);
         webSettings.setAllowContentAccess(true);
         webSettings.setAllowFileAccess(true);
-
-        webView.loadUrl("http://miuportal.manarat.ac.bd/");
-        webView.setWebViewClient(new WebViewClient());
-
         detector = new ConnectionDetector(this);
-
-        detector.connection();        
-
+        loadPortal();
     }
 
+    public void loadPortal() {
+        webView.loadUrl("http://miuportal.manarat.ac.bd/");
+        webView.setWebViewClient(new WebViewClient());
+        detector.connection();
+    }
 
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    public void loadWeb() {
+        webView.loadUrl("http://manarat.ac.bd/");
+        webView.setWebViewClient(new WebViewClient());
+        detector.connection();
     }
 
 }
